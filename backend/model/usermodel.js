a0e19c4640255df3d36cb6e278fab4e67f859eba
khatiwadaprajwal <prajwalkhatiwada28@gmail.com>
2025-12-20 16:47:34 +0545

    model fix

--- backend/model/usermodel.js
+++ backend/model/usermodel.js
@@ -1,16 +1,20 @@
-const mongoose = require("mongoose")
-const UserSchema = new mongoose.Schema({
-    
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["Admin", "Customer","SuperAdmin"], required: true },
+    role: { type: String, enum: ["Admin", "Customer", "SuperAdmin"], required: true },
+    
+    // Security Questions Array
     securityQuestions: [
-    {
-      question: { type: String, required: true },
-      answer: { type: String, required: true, select: false } // HASHED Answer
-    }
-  ],
+      {
+        question: { type: String, required: true },
+        answer: { type: String, required: true, select: false } // HASHED Answer
+      }
+    ],
+    
     loginAttempts: {
         type: Number,
         default: 0,
@@ -18,7 +22,12 @@ const UserSchema = new mongoose.Schema({
     lockUntil: {
         type: Date,
         default: null,
-    }, timestamps: true
-    
-});
+    }
+  }, 
+
+  { 
+    timestamps: true 
+  }
+);
+
 module.exports = mongoose.model('User', UserSchema);
